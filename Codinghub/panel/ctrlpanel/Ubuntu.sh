d29f7fd8ae3336a64a92f5f6b3322d9c90f5febc
𝙏𝙝𝙚 𝘾𝙤𝙙𝙞𝙣𝙜 𝙝𝙪𝙗 <nn8109239@gmail.com>
2026-03-06 21:03:44 +0530

    Update Ubuntu.sh

--- Codinghub/panel/ctrlpanel/Ubuntu.sh
+++ Codinghub/panel/ctrlpanel/Ubuntu.sh
@@ -1,1 +1,19 @@
+# Add "add-apt-repository" command
+apt -y install software-properties-common curl apt-transport-https ca-certificates gnupg
 
+# Add additional repositories for PHP (Ubuntu 20.04 and Ubuntu 22.04 only!!!)
+LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
+
+# Add Redis official APT repository
+curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
+
+# MariaDB repo setup script (Ubuntu 20.04 only!!!)
+curl -LsS https://r.mariadb.com/downloads/mariadb_repo_setup | sudo bash
+
+# Update repositories list
+apt update -y
+
+# Install Dependencies
+apt install -y php8.3 php8.3-{common,cli,gd,mysql,mbstring,bcmath,xml,fpm,curl,zip,intl,redis} mariadb-server nginx git redis-server
+systemctl enable --now redis-server
