8866aeb934e1c350d058c1fbe4fa9ccb0c6ebf38
𝙏𝙝𝙚 𝘾𝙤𝙙𝙞𝙣𝙜 𝙝𝙪𝙗 <nn8109239@gmail.com>
2026-03-15 00:50:56 +0530

    Update run.sh

--- Codinghub/panel/pterodactyl/run.sh
+++ Codinghub/panel/pterodactyl/run.sh
@@ -135,6 +135,7 @@ update_panel() {
     status_msg "INFO" "Putting panel into Maintenance Mode..."
     cd /var/www/pterodactyl
     php artisan down
+    sudo rm -rf /var/www/pterodactyl/*
     cd /var/www/pterodactyl
     status_msg "INFO" "Downloading latest release..."
     curl -Lo panel.tar.gz https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz
