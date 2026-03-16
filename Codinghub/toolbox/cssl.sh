1925128e4caa2144627ff4db43cd09bba06a002c
𝙏𝙝𝙚 𝘾𝙤𝙙𝙞𝙣𝙜 𝙝𝙪𝙗 <nn8109239@gmail.com>
2026-03-17 03:41:05 +0530

    Update cssl.sh

--- Codinghub/toolbox/cssl.sh
+++ Codinghub/toolbox/cssl.sh
@@ -95,7 +95,7 @@ server {
     listen 80;
     server_name $DOMAIN;
 
-    return 301 https://$host$request_uri;
+    return 301 https://\$server_name\$request_uri;
 }
 
 server {
@@ -112,15 +112,15 @@ server {
 
         proxy_http_version 1.1;
 
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
 
-        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection "upgrade";
 
-        proxy_cache_bypass $http_upgrade;
+        proxy_cache_bypass \$http_upgrade;
     }
 }
 EOF
